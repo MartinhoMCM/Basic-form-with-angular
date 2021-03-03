4ea0166200b8d5dc7b25feeca91c2ec5188d8352
MartinhoMCM <martinhocorreiamussamba@gmail.com>
2021-03-03 08:17:47 +0100

    first-comit  Basic Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {User} from './user';
+import {EnrollmentService} from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form-angular';
+ topics=['Angular', 'React', 'Vue'];
+
+isTopicError:boolean=true;
+submitted=false;
+errorMsg='';
+
+ userModel =new User('', 'martinho@gmail.com', 999,'default', 'morning', true);
+
+ constructor(private _enrollmentService: EnrollmentService){}
+
+ validateTopic(value)
+ {
+   this.isTopicError = value=="default"?true:false;
+
+ }
+
+ onSubmit()
+ {
+
+  this.submitted=true;
+   this._enrollmentService.enroll(this.userModel)
+   .subscribe(
+     data=>console.log('Success', data),
+     error =>this.errorMsg=error.statusText 
+    
+   );
+    
+ }
 }
